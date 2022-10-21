5c99c69fd54c80a61bc5a31137b737b609c5d9a0
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-21 17:07:11 -0300

    it at least seems to connect from now

--- main.rb
+++ main.rb
@@ -5,16 +5,15 @@ server = TCPServer.new 2000
 # Setup database
 require 'pg'
 db_type = "PostgreSQL"
-db_name = "JANUSDB"
+db_name = "janusdb"
 db_host = "localhost"
 db_user = "janus"
 db_pswd = "pi"
 
 begin
   # connect to POSTgres server
-  conn = PG.connect(dbname: db_name)
-  server_version = conn.select_one("SELECT VERSION()")
-  puts "Server version: " + row[0]
+  conn = PG.connect(dbname: db_name, user: db_user)
+  puts 'connected'
 ensure
   # disconnect from server
   conn.disconnect if conn
