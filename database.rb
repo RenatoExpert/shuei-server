221cf1f24d2fc46b589b7663a5f08471aeca7fa7
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-27 21:43:56 -0300

    split database functions apart

---
+++ database.rb
@@ -0,0 +1,27 @@
+class Database
+  def create_table (name, *values)
+    values.length > 0 ||  values = ['id int', 'name varchar(255)']
+    $db.execute <<~SQL
+      CREATE TABLE IF NOT EXISTS #{name}(
+        #{values.join(", ")}
+      );
+    SQL
+  end
+
+  def insert_row (table, columns, *values)
+    col_list = columns ? "('#{columns.join("', '")}')" : ' '
+    values = values.join("', '")
+    p values
+    $db.execute "INSERT INTO #{table}  #{col_list} VALUES ('#{values}')"
+  end
+
+  def insert_log (timestamp, devuid, devaddr, priority, message)
+    insert_row 'logs', ['timestamp', 'devuid', 'devaddr', 'priority', 'message'], timestamp, devuid, devaddr, priority, message
+  end
+
+  def setup()
+    create_table 'slaves', 'huuid', 'tagname', 'curIP', 'GPIO_Status'
+    create_table 'logs', 'ID INTEGER PRIMARY KEY AUTOINCREMENT', 'timestamp TEXT', 'devuid TEXT', 'devaddr TEXT', 'priority TEXT', 'message TEXT'
+  end
+end
+
--- main.rb
+++ main.rb
@@ -1,24 +1,4 @@
-
-def create_table (name, *values)
-  values.length > 0 ||  values = ['id int', 'name varchar(255)']
-  $db.execute <<~SQL
-    CREATE TABLE IF NOT EXISTS #{name}(
-      #{values.join(", ")}
-    );
-  SQL
-end
-
-def insert_row (table, columns, *values)
-  col_list = columns ? "('#{columns.join("', '")}')" : ' '
-  values = values.join("', '")
-  p values
-  $db.execute "INSERT INTO #{table}  #{col_list} VALUES ('#{values}')"
-end
-
-def insert_log (timestamp, devuid, devaddr, priority, message)
-  insert_row 'logs', ['timestamp', 'devuid', 'devaddr', 'priority', 'message'], timestamp, devuid, devaddr, priority, message
-end
-
+require_relative "database"
 
 BEGIN {
   # Setup Gems
@@ -46,10 +26,6 @@ BEGIN {
   require 'json'
 
 }
-
-create_table 'slaves', 'huuid', 'tagname', 'curIP', 'GPIO_Status'
-create_table 'logs', 'ID INTEGER PRIMARY KEY AUTOINCREMENT', 'timestamp TEXT', 'devuid TEXT', 'devaddr TEXT', 'priority TEXT', 'message TEXT'
-
 
 END {
   loop do
