36a45f7a7b3b76c15648aebb608b3ab633671e39
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-27 21:58:02 -0300

    using @db instead of $db

--- database.rb
+++ database.rb
@@ -1,14 +1,14 @@
 require 'sqlite3'
 class Database
   def initialize (dbpath)
-    $db = SQLite3::Database.open(dbpath)
+    @db = SQLite3::Database.open(dbpath)
     create_table 'slaves', 'huuid', 'tagname', 'curIP', 'GPIO_Status'
     create_table 'logs', 'ID INTEGER PRIMARY KEY AUTOINCREMENT', 'timestamp TEXT', 'devuid TEXT', 'devaddr TEXT', 'priority TEXT', 'message TEXT'
   end
 
   def create_table (name, *values)
     values.length > 0 ||  values = ['id int', 'name varchar(255)']
-    $db.execute <<~SQL
+    @db.execute <<~SQL
       CREATE TABLE IF NOT EXISTS #{name}(
         #{values.join(", ")}
       );
@@ -19,7 +19,7 @@ class Database
     col_list = columns ? "('#{columns.join("', '")}')" : ' '
     values = values.join("', '")
     p values
-    $db.execute "INSERT INTO #{table}  #{col_list} VALUES ('#{values}')"
+    @db.execute "INSERT INTO #{table}  #{col_list} VALUES ('#{values}')"
   end
 
   def insert_log (timestamp, devuid, devaddr, priority, message)
